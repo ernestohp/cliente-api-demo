73594c967631123e821e57e804a52696734c95e8
Huari Perez <e-eohu@gft.com>
2023-07-31 19:27:31 +0200

    first commit

---
+++ receivefile.py
@@ -0,0 +1,25 @@
+from flask import Flask, request, jsonify
+import os
+
+app = Flask(__name__)
+
+UPLOAD_FOLDER = 'uploads'  # The folder where uploaded files will be stored
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    if 'file' not in request.files:
+        return jsonify({'error': 'No file part in the request'}), 400
+
+    file = request.files['file']
+
+    if file.filename == '':
+        return jsonify({'error': 'No selected file'}), 400
+
+    if file:
+        filename = file.filename
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        return jsonify({'message': 'File uploaded successfully'}), 200
+
+if __name__ == '__main__':
+    app.run(debug=True)
