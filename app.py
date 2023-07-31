2a67dda9fd229c7120dfab15ccd72c86d1e9964d
Huari Perez <e-eohu@gft.com>
2023-07-31 20:19:01 +0200

    rename to app.py

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, request, jsonify
+import os
+
+app = Flask(__name__)
+
+UPLOAD_FOLDER = 'uploads'  # The folder where uploaded files will be stored
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    if 'file' not in request.files:
+        return jsonify({'error': 'No file part in the request'}), 400
+
+    file = request.files['file']
+
+    if file.filename == '':
+        return jsonify({'error': 'No selected file'}), 400
+
+    if file:
+        # filename = file.filename
+        # file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        return jsonify({'message': 'File uploaded successfully'}), 200
+
+@app.route('/hola', methods=['GET'])
+def hola():
+    return jsonify({'message': 'Hola Mundo'}), 200
+
+@app.route('/', methods=['GET'])
+def hola2():
+    return "Prueba ok", 200
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
